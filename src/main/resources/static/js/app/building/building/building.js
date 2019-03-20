bb7ff167291f714064bb030353132fa5ec079d47
fengwang <76529775@qq.com>
2019-03-20 17:11:30 +0800

    调整

---
+++ src/main/resources/static/js/app/building/building/building.js
@@ -0,0 +1,137 @@
+(function ($) {
+    $.extend({
+        building: {}
+    });
+
+    /**
+     * 表格内容
+     */
+    $.building.tableColumns = [{
+        field: 'dsRegionName',
+        title: '地市'
+    }, {
+        field: 'regionName',
+        title: '区县'
+    }, {
+        field: 'buildingNo',
+        title: '楼宇编码'
+    }, {
+        field: 'buildingName',
+        title: '楼宇名称'
+    }, {
+        field: 'buildingLevelName',
+        title: '楼宇等级'
+    }, {
+        field: 'buildingTypeDesc',
+        title: '楼宇所属区域类型'
+    }, {
+        field: 'buildingTypeName',
+        title: '楼宇所属区域名称'
+    }, {
+        field: 'buildingAddress',
+        title: '楼宇地址'
+    }, {
+        field: 'longitude',
+        title: '经度'
+    }, {
+        field: 'latitude',
+        title: '纬度'
+    }, {
+        field: 'floorNumber',
+        title: '楼层数'
+    }, {
+        field: 'area',
+        title: '建筑面积（万m2）'
+    }, {
+        field: 'householderNumber',
+        title: '商户数（户）'
+    }, {
+        field: 'propertyCompany',
+        title: '物业单位'
+    }, {
+        field: 'propertyManager',
+        title: '物业联系人'
+    }, {
+        field: 'propertyManagerContact',
+        title: '物业联系人电话'
+    }, {
+        field: 'buildingManager',
+        title: '楼长'
+    }, {
+        field: 'buildingManagerContact',
+        title: '楼长电话'
+    }, {
+        field: 'stateName',
+        title: '楼宇状态'
+    }, {
+        field: 'remark',
+        title: '备注'
+    }, {
+        field: 'standName',
+        title: '标准地址'
+    }, {
+        field: 'accessAreaName',
+        title: '综合业务接入区'
+    }, {
+        field: 'roomName',
+        title: '综合业务接入机房'
+    }, {
+        field: 'lightName',
+        title: '光交名称'
+    }, {
+        field: 'lightAddress',
+        title: '光交地址'
+    }, {
+        field: 'lightLongitude',
+        title: '光交经度'
+    }, {
+        field: 'lightLatitude',
+        title: '光交纬度'
+    }, {
+        field: 'roomCabinetFlagName',
+        title: '是否有机房/机柜'
+    }, {
+        field: 'cableAccessFlagName',
+        title: '光缆资源是否接入'
+    }, {
+        field: 'cableNumber',
+        title: '进楼光缆条数'
+    }, {
+        field: 'cableRemainNumber',
+        title: '空余芯数'
+    }, {
+        field: 'verticalCoverageFlagName',
+        title: '是否具备垂直覆盖'
+    }, {
+        field: 'horizontalCoverageFlagName',
+        title: '是否具备水平覆盖'
+    }, {
+        field: 'coverageMethodName',
+        title: '楼宇覆盖方式'
+    }, {
+        field: 'cooperationMethodName',
+        title: '合作方式'
+    }, {
+        field: 'indoorDistributionFlagName',
+        title: '是否具备室分'
+    }, {
+        field: 'coverageNetworkStandardName',
+        title: '已覆盖网络制式'
+    }, {
+        field: 'wlanFlagName',
+        title: 'WLAN是否已覆盖'
+    }, {
+        field: 'createStaffName',
+        title: '录入人'
+    }, {
+        field: 'createDate',
+        title: '录入时间'
+    }];
+
+    /**
+     * 初始化
+     */
+    $.building.init = function () {
+
+    };
+})(jQuery);
--- src/main/resources/static/js/app/building/building/index.js
+++ src/main/resources/static/js/app/building/building/index.js
@@ -2,148 +2,47 @@ $(function () {
     var $form = $(".building-table-form");
     var $ds = $form.find("select[name='dsRegionId']");
     var $qx = $form.find("select[name='regionId']");
+    var $selectMode = $("#buildingTable").attr("selectMode");
 
-    var settings = {
-        url: ctx + "building/list",
-        pageSize: 10,
-        queryParams: function (params) {
-            return {
-                pageSize: params.limit,
-                pageNum: params.offset / params.limit + 1,
-                dsRegionId: $ds.val(),
-                regionId: $qx.val(),
-                buildingNo: $form.find("input[name='buildingNo']").val().trim(),
-                buildingName: $form.find("input[name='buildingName']").val().trim(),
-                buildingType: $form.find("select[name='buildingType']").val(),
-                buildingTypeName: $form.find("input[name='buildingTypeName']").val().trim(),
-                buildingAddress: $form.find("input[name='buildingAddress']").val().trim(),
-                state: $form.find("select[name='state']").val()
-            };
-        },
-        columns: [{
-            checkbox: true
-        }, {
-            field: 'dsRegionName',
-            title: '地市'
-        }, {
-            field: 'regionName',
-            title: '区县'
-        }, {
-            field: 'buildingNo',
-            title: '楼宇编码'
-        }, {
-            field: 'buildingName',
-            title: '楼宇名称'
-        }, {
-            field: 'buildingLevelName',
-            title: '楼宇等级'
-        }, {
-            field: 'buildingTypeDesc',
-            title: '楼宇所属区域类型'
-        }, {
-            field: 'buildingTypeName',
-            title: '楼宇所属区域名称'
-        }, {
-            field: 'buildingAddress',
-            title: '楼宇地址'
-        }, {
-            field: 'longitude',
-            title: '经度'
-        }, {
-            field: 'latitude',
-            title: '纬度'
-        }, {
-            field: 'floorNumber',
-            title: '楼层数'
-        }, {
-            field: 'area',
-            title: '建筑面积（万m2）'
-        }, {
-            field: 'householderNumber',
-            title: '商户数（户）'
-        }, {
-            field: 'propertyCompany',
-            title: '物业单位'
-        }, {
-            field: 'propertyManager',
-            title: '物业联系人'
-        }, {
-            field: 'propertyManagerContact',
-            title: '物业联系人电话'
-        }, {
-            field: 'buildingManager',
-            title: '楼长'
-        }, {
-            field: 'buildingManagerContact',
-            title: '楼长电话'
-        }, {
-            field: 'stateName',
-            title: '楼宇状态'
-        }, {
-            field: 'remark',
-            title: '备注'
-        }, {
-            field: 'standName',
-            title: '标准地址'
-        }, {
-            field: 'accessAreaName',
-            title: '综合业务接入区'
-        }, {
-            field: 'roomName',
-            title: '综合业务接入机房'
-        }, {
-            field: 'lightName',
-            title: '光交名称'
-        }, {
-            field: 'lightAddress',
-            title: '光交地址'
-        }, {
-            field: 'lightLongitude',
-            title: '光交经度'
-        }, {
-            field: 'lightLatitude',
-            title: '光交纬度'
-        }, {
-            field: 'roomCabinetFlagName',
-            title: '是否有机房/机柜'
-        }, {
-            field: 'cableAccessFlagName',
-            title: '光缆资源是否接入'
-        }, {
-            field: 'cableNumber',
-            title: '进楼光缆条数'
-        }, {
-            field: 'cableRemainNumber',
-            title: '空余芯数'
-        }, {
-            field: 'verticalCoverageFlagName',
-            title: '是否具备垂直覆盖'
-        }, {
-            field: 'horizontalCoverageFlagName',
-            title: '是否具备水平覆盖'
-        }, {
-            field: 'coverageMethodName',
-            title: '楼宇覆盖方式'
-        }, {
-            field: 'cooperationMethodName',
-            title: '合作方式'
-        }, {
-            field: 'indoorDistributionFlagName',
-            title: '是否具备室分'
-        }, {
-            field: 'coverageNetworkStandardName',
-            title: '已覆盖网络制式'
-        }, {
-            field: 'wlanFlagName',
-            title: 'WLAN是否已覆盖'
-        }, {
-            field: 'createStaffName',
-            title: '录入人'
-        }, {
-            field: 'createDate',
-            title: '录入时间'
-        }]
-    };
+    function initTable() {
+        var pageSize = 10;
+        var columns = [];
+
+        if ($.utils.selectModeSingle === $selectMode) {
+            pageSize = 5;
+            var checkboxColumns = [{
+                checkbox: true
+            }];
+            columns = checkboxColumns.concat($.building.tableColumns);
+        } else if ($.utils.selectModeMultiple === $selectMode) {
+            pageSize = 5;
+        } else {
+            pageSize = 10;
+            columns = $.building.tableColumns;
+        }
+
+        var settings = {
+            url: ctx + "building/list",
+            pageSize: pageSize,
+            queryParams: function (params) {
+                return {
+                    pageSize: params.limit,
+                    pageNum: params.offset / params.limit + 1,
+                    dsRegionId: $ds.val(),
+                    regionId: $qx.val(),
+                    buildingNo: $form.find("input[name='buildingNo']").val().trim(),
+                    buildingName: $form.find("input[name='buildingName']").val().trim(),
+                    buildingType: $form.find("select[name='buildingType']").val(),
+                    buildingTypeName: $form.find("input[name='buildingTypeName']").val().trim(),
+                    buildingAddress: $form.find("input[name='buildingAddress']").val().trim(),
+                    state: $form.find("select[name='state']").val()
+                };
+            },
+            columns: columns
+        };
+
+        $MB.initTable('buildingTable', settings);
+    }
 
     function search() {
         $MB.refreshTable('buildingTable');
@@ -201,7 +100,32 @@ $(function () {
         });
     }
 
-    $MB.initTable('buildingTable', settings);
+    function selectOk() {
+        var selected = $("#buildingTable").bootstrapTable('getSelections');
+        var selected_length = selected.length;
+        if (!selected_length) {
+            $MB.n_warning('请勾选需要选择的楼宇！');
+            return;
+        }
+        if (selected_length > 1) {
+            $MB.n_warning('一次只能选择一个楼宇！');
+            return;
+        }
+        var buildingId = selected[0].buildingId;
+        var buildingName = selected[0].buildingName;
+
+        console.log("buildingId:" + buildingId + ", buildingName:" + buildingName);
+
+        var selectBackFormId = $("#building-select-modal").attr("selectBackFormId");
+        if (selectBackFormId) {
+            $("#" + selectBackFormId).find("input[name='buildingId']").val(buildingId);
+            $("#" + selectBackFormId).find("input[name='buildingName']").val(buildingName);
+        }
+
+        $('#building-select-modal').modal('hide');
+    }
+
+    initTable();
 
     $(".building-search").click(function () {
         search();
@@ -217,6 +141,10 @@ $(function () {
 
     $(".building-export-csv").click(function () {
         $.utils.exportFile(ctx + "building/csv", $form.serialize());
+    });
+
+    $(".building-select-modal-ok").click(function () {
+        selectOk();
     });
 
     $(".building-add").click(function () {
